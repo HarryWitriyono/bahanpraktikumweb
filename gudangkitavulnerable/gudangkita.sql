8c9d10025f870e92baff02a9e50c6be0c045b5b6
Harry Witriyono <49424961+HarryWitriyono@users.noreply.github.com>
2024-06-14 13:34:01 +0700

    Add files via upload

--- gudangkitavulnerable/gudangkita.sql
+++ gudangkitavulnerable/gudangkita.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2023 at 01:04 PM
+-- Generation Time: Jun 14, 2024 at 08:33 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.14
 
@@ -36,6 +36,14 @@ CREATE TABLE `barang` (
   `TglAuditTerakhir` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`KodeBarang`, `NamaBarang`, `JumlahStok`, `Harga`, `Satuan`, `TglAuditTerakhir`) VALUES
+('4970129727514', 'Spidol Boardmarker SNOWMAN Warna Hitam', 280.00, 0.00, 'pak', '2023-12-11 00:00:00'),
+('8901057510028', 'Staples Kangoro No.10-1M', 200.00, 0.00, 'pak', '2023-12-11 00:00:00');
+
 -- --------------------------------------------------------
 
 --
@@ -52,6 +60,17 @@ CREATE TABLE `barangdigudang` (
   `KodeBarang` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `barangdigudang`
+--
+
+INSERT INTO `barangdigudang` (`IdTransaksi`, `WaktuTransaksi`, `StatusTransaksi`, `Jumlah`, `Keterangan`, `KodeGudang`, `KodeBarang`) VALUES
+(1, '2023-12-11 00:00:00', 'Masuk', 100.00, '', 3, '4970129727514'),
+(2, '2023-12-11 00:00:00', 'Masuk', 100.00, '', 3, '8901057510028'),
+(3, '2023-12-11 00:00:00', 'Masuk', 200.00, '', 4, '4970129727514'),
+(4, '2023-12-11 00:00:00', 'Keluar', 20.00, '', 3, '4970129727514'),
+(5, '2023-12-11 00:00:00', 'Masuk', 100.00, '', 4, '8901057510028');
+
 -- --------------------------------------------------------
 
 --
@@ -62,6 +81,14 @@ CREATE TABLE `gudang` (
   `KodeGudang` int(11) NOT NULL,
   `Alamat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `gudang`
+--
+
+INSERT INTO `gudang` (`KodeGudang`, `Alamat`) VALUES
+(3, 'Gudang 1'),
+(4, 'Gudang 2');
 
 -- --------------------------------------------------------
 
@@ -74,8 +101,18 @@ CREATE TABLE `pengguna` (
   `Password` varchar(30) NOT NULL,
   `NamaPengguna` varchar(50) NOT NULL,
   `Alamat` text DEFAULT NULL,
-  `waktudaftar` datetime DEFAULT current_timestamp()
+  `waktudaftar` datetime DEFAULT current_timestamp(),
+  `level` enum('Admin','Operator','Umum') NOT NULL DEFAULT 'Umum'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengguna`
+--
+
+INSERT INTO `pengguna` (`KodeLogin`, `Password`, `NamaPengguna`, `Alamat`, `waktudaftar`, `level`) VALUES
+('Admin', 'Admin', '', NULL, '2023-12-11 14:05:48', 'Admin'),
+('Operator', 'Operator', '', NULL, '2023-12-11 14:23:55', 'Operator'),
+('Umum', 'Umum', '', NULL, '2023-12-11 14:24:04', 'Umum');
 
 --
 -- Indexes for dumped tables
@@ -115,13 +152,13 @@ ALTER TABLE `pengguna`
 -- AUTO_INCREMENT for table `barangdigudang`
 --
 ALTER TABLE `barangdigudang`
-  MODIFY `IdTransaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `IdTransaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `gudang`
 --
 ALTER TABLE `gudang`
-  MODIFY `KodeGudang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `KodeGudang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
